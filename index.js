7c58713d808c4f2366ab9385101dde73b617f0a3
Yuval Dahan <dahan236@gmail.com>
2023-09-02 16:49:43 +0300

    Udemy - web developer drum kit project

---
+++ index.js
@@ -0,0 +1,71 @@
+// passing over throw each selector that takes part of class ".drum".
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfDrumButtons; i++){
+
+    //adding an EventListener such as "click" using "addEventListener". 
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+// Finding the currentKey that triggered and using 'switch' to activate it.
+function makeSound(key) {
+    var buttonInnerHTML = this.innerHTML;
+        switch(key) {
+            case "w": 
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+            case "a": 
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+            case "s": 
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+            case "d": 
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+            case "j": 
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+            case "k": 
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+            case "l": 
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+            default:
+                console.log(buttonInnerHTML);
+        };
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    // Adding and Removing a timeout to each press that triggered.
+    activeButton.classList.add("pressed");
+    setTimeout(function () {
+        activeButton.classList.remove("pressed")
+    }, 100);
+}
+
